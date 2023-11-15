d4a46c391f76f13d0e7789b2b18c0af356dd268a
BulatRuslanovich <bulatruslanovich@gmail.com>
2023-11-15 22:20:17 +0300

    fix: add little fix

--- Item.cs
+++ Item.cs
@@ -1,20 +1,18 @@
 namespace MyDictionary {
     public class Item<TKey, TValue> {
-		public TKey Key { get; set; }
-		public TValue Value { get; set; }
-		public Item(TKey key, TValue value) {
-			Key = key;
-			Value = value;
-		}
+        public TKey Key { get; set; }
+        public TValue Value { get; set; }
+        public Item(TKey key, TValue value) {
+            Key = key;
+            Value = value;
+        }
 
-		public override int GetHashCode() {
-			return Key.GetHashCode();
-		}
+        public override int GetHashCode() {
+            return Key.GetHashCode();
+        }
 
-		public override string ToString() {
-			return Value.ToString();
-		}
-
-
-	}
+        public override string ToString() {
+            return Value.ToString();
+        }
+    }
 }
--- MyMap.cs
+++ MyMap.cs
@@ -1,71 +1,166 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Threading.Tasks;
+using System.Collections;
 
-namespace MyDictionary
-{
-    public class MyMap<TKey, TValue>
-    {
-		private int size = 100;
+namespace MyDictionary {
+    public class MyMap<TKey, TValue> : IEnumerable {
+        private int size = 100;
         private Item<TKey, TValue>[] Items;
-		public MyMap()
-		{
-			Items = new Item<TKey, TValue>[100];
-		}
+        private List<TKey> Keys = new List<TKey>();
+        public MyMap() {
+            Items = new Item<TKey, TValue>[100];
+        }
 
-		public void Add(Item<TKey, TValue> item) {
-			var hash = GetHash(item.Key);
+        public void Add(Item<TKey, TValue> item) {
+            var hash = GetHash(item.Key);
 
-			if (Items[hash] == null) {
-				Items[hash] = item;
-			} else {
-				var placed = false;
-				for (int i = hash; i < size; ++i) {
-					if (Items[i].Key.Equals(item.Key)) {
-						return;
-					}
+            if (Keys.Contains(item.Key)) {
+                return;
+            }
 
-					if (Items[i] == null) {
-						Items[i] = item;
-						placed = true;
-						break;
-					}
-				}
+            if (Items[hash] == null) {
+                Keys.Add(item.Key);
+                Items[hash] = item;
+            } else {
+                var placed = false;
+                for (int i = hash; i < size; ++i) {
+                    if (Items[i] == null) {
+                        Keys.Add(item.Key);
+                        Items[i] = item;
+                        placed = true;
+                        break;
+                    }
 
-				if (!placed) {
-					for (int i = 0; i < hash; ++i) {
-						if (Items[i].Key.Equals(item.Key)) {
-							return;
-						}
+                    // if (Items[i].Key.Equals(item.Key)) {
+                    //     return;
+                    // }
+                }
 
-						if (Items[i] == null) {
-							Items[i] = item;
-							placed = true;
-							break;
-						}
-					}
-				}
+                if (!placed) {
+                    for (int i = 0; i < hash; ++i) {
+                        if (Items[i] == null) {
+                            Keys.Add(item.Key);
+                            Items[i] = item;
+                            placed = true;
+                            break;
+                        }
 
-				if (!placed) {
-					throw new Exception("Map is full");
-				}
-			}
-		}
+                        // if (Items[i].Key.Equals(item.Key)) {
+                        //     return;
+                        // }
+                    }
+                }
 
-		public void Remove(TKey key) {
-			var hash = GetHash(key);
+                if (!placed) {
+                    throw new Exception("Map is full");
+                }
+            }
+        }
 
-			if (Items[hash].Key.Equals(key)) {
-				Items[hash] = null;
-			} else {
-				
-			}
-		}
+        public IEnumerator GetEnumerator() {
+            foreach (var item in Items) {
+                if (item != null) {
+                    yield return item;
+                }
+            }
+        }
 
-		private int GetHash(TKey key) {
-			return key.GetHashCode() % size;
-		}
+        public void Remove(TKey key) {
+            var hash = GetHash(key);
+
+            if (!Keys.Contains(key)) {
+                return;
+            }
+
+            if (Items[hash] == null) {
+                for (int i = 0; i < size; i++) {
+                    if (Items[i].Key.Equals(key)) {
+                        Items[i] = null;
+                        Keys.Remove(key);
+                    }
+                }
+
+                return;
+            }
+
+            if (Items[hash].Key.Equals(key)) {
+                Items[hash] = null;
+                Keys.Remove(key);
+            } else {
+                var placed = false;
+                for (int i = hash; i < size; ++i) {
+                    if (Items[i] == null) {
+                        return;
+                    }
+
+                    if (Items[i] != null && Items[i].Key.Equals(key)) {
+                        Items[hash] = null;
+                        Keys.Remove(key);
+                        return;
+                    }
+                }
+
+                if (!placed) {
+                    for (int i = 0; i < hash; ++i) {
+                        if (Items[i] == null) {
+                            return;
+                        }
+
+                        if (Items[i].Key.Equals(key)) {
+                            Items[hash] = null;
+                            Keys.Remove(key);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        public TValue Search(TKey key) {
+            var hash = GetHash(key);
+
+            if (!Keys.Contains(key)) {
+                return default;
+            }
+
+            if (Items[hash] == null) {
+                foreach (var item in Items) {
+                    if (item.Key.Equals(key)) {
+                        return item.Value;
+                    }
+                }
+            }
+
+            if (Items[hash].Key.Equals(key)) {
+                return Items[hash].Value;
+            } else {
+                var placed = false;
+                for (int i = hash; i < size; ++i) {
+                    if (Items[i] == null) {
+                        return default;
+                    }
+
+                    if (Items[i].Key.Equals(key)) {
+                        return Items[hash].Value;
+                    }
+                }
+
+                if (!placed) {
+                    for (int i = 0; i < hash; ++i) {
+                        if (Items[i] == null) {
+                            return default;
+                        }
+
+                        if (Items[i].Key.Equals(key)) {
+                            return Items[hash].Value;
+                        }
+                    }
+                }
+
+                return default;
+            }
+        }
+
+        private int GetHash(TKey key) {
+            return key.GetHashCode() % size;
+        }
     }
 }
